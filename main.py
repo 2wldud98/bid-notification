41ecf75220726b0a61e2432ae7a5de2d66952126
jiyeong <2wldud98@gmail.com>
2025-07-16 22:50:12 +0900

    feat: 배치 시간 기준으로 공고게시일 요청 파라미터 설정
    - 배치 실행 시각을 기준으로 inqryBgnDt, inqryEndDt 계산

--- main.py
+++ main.py
@@ -2,10 +2,44 @@ import json
 import requests
 import os
 from dotenv import load_dotenv
+from datetime import datetime, timedelta
 
 # 환경변수 불러오기
 load_dotenv()
 SERVICE_KEY = os.getenv('SERVICE_KEY')
+
+# 기준 배치 시각 설정 (9,12,15,18시)
+BATCH_TIMES = [9, 12, 15, 18]
+
+# 배치 시간대 설정 함수
+def get_batch_time_ranges(now):
+    # 현재 시각보다 같거나 이전인 배치 시각 필터링
+    valid_times = [h for h in BATCH_TIMES if h <= now.hour]
+    # case 1: 현재 시각이 가장 이른 배치 이전일 경우 (ex. 08:30 등)
+    if not valid_times:
+        first_batch_hour = BATCH_TIMES[0]
+        last_batch_hour = BATCH_TIMES[-1]
+
+        prev_day = now - timedelta(days=1)
+        # 전날 마지막 배치 시각부터 오늘 첫 배치 시각까지
+        bgn = prev_day.replace(hour=last_batch_hour, minute=0, second=0, microsecond=0)
+        end = now.replace(hour=first_batch_hour, minute=0, second=0, microsecond=0)
+        return bgn.strftime("%Y%m%d%H%M"), end.strftime("%Y%m%d%H%M")
+
+    # case 2: 현재 시각이 배치 시간 이후일 경우 → 가장 가까운 이전 배치 구간 반환
+    prev_batch_hour = max(valid_times)
+    idx = BATCH_TIMES.index(prev_batch_hour)
+
+    if idx == 0:
+        # 첫 번째 배치인 경우: 전날 마지막 배치 시각부터 오늘 첫 배치 시각까지
+        bgn = now.replace(hour=BATCH_TIMES[-1], minute=0, second=0, microsecond=0) - timedelta(days=1)
+    else:
+        # 일반 배치 구간 (이전 배치 시각 → 현재 배치 시각)
+        bgn = now.replace(hour=BATCH_TIMES[idx-1], minute=0, second=0, microsecond=0)
+
+    end = now.replace(hour=prev_batch_hour, minute=0, second=0, microsecond=0)
+
+    return bgn.strftime("%Y%m%d%H%M"), end.strftime("%Y%m%d%H%M")
 
 # 발송 이력 파일 경로
 SENT_FILE = "sent_notifications.json"
@@ -20,6 +54,13 @@ else:
 # 사용자 JSON 파일 읽기
 with open("users.json", "r", encoding="utf-8") as f:
     users = json.load(f)
+
+# 배치 시간 구간 계산
+now = datetime.now()
+#now = datetime.now().replace(hour=8, minute=59, second=0, microsecond=0)
+print("now: ", now)
+inqryBgnDt, inqryEndDt = get_batch_time_ranges(now)
+print(f"[배치 요청 시간 범위] {inqryBgnDt} ~ {inqryEndDt}")
 
 # 사용자별 키워드 기반 API 요청
 for user in users:
@@ -37,8 +78,8 @@ for user in users:
             "pageNo": 1,
             "numOfRows": 10,
             "inqryDiv": 1,
-            "inqryBgnDt": "202506160000",
-            "inqryEndDt": "202507152359",
+            "inqryBgnDt": inqryBgnDt,
+            "inqryEndDt": inqryEndDt,
             "bidNtceNm": keyword,
             "type": "json"
         }
